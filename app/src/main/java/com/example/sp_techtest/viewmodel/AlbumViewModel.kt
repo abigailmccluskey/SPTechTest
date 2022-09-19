36267d1ea0c22d03181b3088a5290f501ed5137d
multivac <davidmccluskey1@gmail.com>
2022-09-19 22:27:53 +0100

    Create AlbumViewModel.kt

---
+++ app/src/main/java/com/example/sp_techtest/viewmodel/AlbumViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.sp_techtest.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sp_techtest.model.Album
+import kotlinx.coroutines.launch
+
+class AlbumViewModel : ViewModel() {
+    var albumList:List<Album> by mutableStateOf(listOf())
+    var errorMessage: String by mutableStateOf("")
+    fun getAlbumList() {
+        viewModelScope.launch {
+            val apiService = AlbumAPIService.getInstance().create(AlbumAPIService::class.java)
+            try {
+                albumList = apiService.getAlbums()
+            }
+            catch (e: Exception) {
+                errorMessage = e.message.toString()
+            }
+        }
+    }
+}
