bd9ae73986c5fb6f0c94fbcd000ab04f2520ca63
multivac <davidmccluskey1@gmail.com>
2022-09-19 23:23:02 +0100

    Displaying album list

--- app/src/main/java/com/example/sp_techtest/MainActivity.kt
+++ app/src/main/java/com/example/sp_techtest/MainActivity.kt
@@ -3,16 +3,30 @@ package com.example.sp_techtest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sp_techtest.model.Album
 import com.example.sp_techtest.ui.theme.SPTechTestTheme
+import com.example.sp_techtest.viewmodel.AlbumViewModel
+import android.util.Log
+import kotlin.math.log
+
 
 class MainActivity : ComponentActivity() {
+    val albumVM by viewModels<AlbumViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +36,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("World")
+                    AlbumList(albumList = albumVM.albumList)
+                    albumVM.getAlbumList()
                 }
             }
         }
@@ -30,14 +45,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AlbumList(albumList: List<Album>) {
+    LazyColumn {
+        itemsIndexed(items = albumList) { index, item ->
+            AlbumItem(album = item)
+        }
+    }
+}
+
+@Composable
+fun AlbumItem(album: Album) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp, 4.dp)
+            .fillMaxWidth()
+            .height(50.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
+    ) {
+        Surface(color = Color.White) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxHeight()
+            ) {
+                Text(text = "${album.title}")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SPTechTestTheme {
-        Greeting("Android")
-    }
+fun AlbumPreview() {
+    val album = Album(
+        1,
+        1,
+        "My Beautiful Dark Twisted Fantasy",
+    )
+    AlbumItem(album = album)
 }
--- app/src/main/java/com/example/sp_techtest/model/AlbumModel.kt
+++ app/src/main/java/com/example/sp_techtest/model/AlbumModel.kt
@@ -1,7 +1,7 @@
 package com.example.sp_techtest.model
 
-sealed class Album(userID: Int, id: Int, title: String) {
-    var user_id: Int = userID
-    var album_id: Int = id
-    var album_title: String = title
+data class Album(val user_id: Int, val album_id: Int, val album_title: String) {
+    var userID: Int = user_id
+    var id: Int = album_id
+    var title: String = album_title
 }
